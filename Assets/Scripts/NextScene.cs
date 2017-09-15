3a5c033adcc6511f2ca9143c29e855d102a8ee34
Stijn Seghers <stijnseghers@gmail.com>
2017-09-15 19:39:53 +1200

    Make start, main and end scenes
    You can transition between them by pressing space.

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    public string triggerKey;
+    public string nextSceneName;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown (triggerKey)) {
+            SceneManager.LoadScene (nextSceneName);
+        }
+    }
+}
