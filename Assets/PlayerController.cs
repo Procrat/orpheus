0111e50bc62e0fe099fd8ac169390b59f166bb77
Stijn Seghers <stijnseghers@gmail.com>
2017-09-15 20:03:49 +1200

    Add a controllable checkmark player ><

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D body;
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate ()
+    {
+        HandleInput ();
+    }
+
+    private void HandleInput ()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.Q)) {
+            Debug.Log ("Quitting.");
+            Application.Quit ();
+        }
+
+        if (Input.GetKey (KeyCode.RightArrow)) {
+            MoveRight ();
+        } else if (Input.GetKey (KeyCode.LeftArrow)) {
+            MoveLeft ();
+        }
+
+        // Temporary shortcut to win/die
+        if (Input.GetKeyDown ("space")) {
+            Win ();
+        }
+    }
+
+    private void MoveLeft ()
+    {
+        Move (Vector2.left);
+    }
+
+    private void MoveRight ()
+    {
+        Move (Vector2.right);
+    }
+
+    private void Move (Vector2 translationVector)
+    {
+        transform.Translate (moveSpeed * translationVector);
+    }
+
+    public void Win ()
+    {
+        Debug.Log ("Hooray! You won!");
+        SceneManager.LoadScene ("End");
+    }
+}
