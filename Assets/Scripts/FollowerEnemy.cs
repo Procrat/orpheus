d65b1e7a2fef7728f784ec6024b37a182116cbd3
INVOKER\VEC <dylan.kumar@icloud.com>
2017-09-16 13:19:34 +1200

    added a new NPC type called Runner

---
+++ Assets/Scripts/FollowerEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerEnemy : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < player.transform.position.x) {
+			transform.Translate (0.1f, 0, 0);
+		}
+	}
+}
