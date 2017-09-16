3ddcacb99997a7247f1629a5c9dbaa3acd7d0e97
INVOKER\VEC <dylan.kumar@icloud.com>
2017-09-16 20:13:04 +1200

    tryingout parallax

--- Assets/Scripts/FollowAbility.cs
+++ Assets/Scripts/FollowAbility.cs
@@ -3,24 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowAbility : MonoBehaviour {
-	
+
+
+	public float moveSpeed;
+	public float feetWidth;
+
 	private GameObject player;
+
+	public LayerMask whatIsGround;	
+	private Rigidbody2D body;
+
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
+		body = GetComponent<Rigidbody2D> ();	
 	}
 
 	// Update is called once per frame
 	public void Move () {
 		
 		if (transform.position.x < player.transform.position.x) {
-			transform.Translate (0.01f, 0, 0);
+			transform.Translate (0.05f, 0, 0);
 		}
 
 		if (transform.position.x > player.transform.position.x) {
-			transform.Translate (-0.01f, 0, 0);
+			transform.Translate (-0.05f, 0, 0);
 		}
+
 	}
 
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "deadlyOnTouch") {
+			Jump ();
+		}
+	}
+		
+
+	private void Jump (){
+		float jumpSpeed = 12;
+		body.velocity = jumpSpeed * Vector2.up;
+	}
 }
+
