f97613973a496111aec429edd618096778ad9771
Myrjamvdv <myrjamvdv@gmail.com>
2017-09-17 12:35:15 +1200

    Sad cutscene at start

---
+++ Assets/Scripts/StartLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLevel : MonoBehaviour
+{
+	public float startDelay;
+
+	void Start ()
+	{
+		StartCoroutine (StartAfterDelay ());
+	}
+
+	IEnumerator StartAfterDelay ()
+	{
+		yield return new WaitForSeconds (startDelay);
+		SceneManager.LoadScene ("Start");
+	}
+}
