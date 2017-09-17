c436af79c664d684596053a2e2f4f54d7333b71f
Stijn Seghers <stijnseghers@gmail.com>
2017-09-17 15:55:00 +1200

    Level 0 -> 0,5 transition

---
+++ Assets/Scripts/LoadSceneOnTouch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTouch : MonoBehaviour {
+	public string scene;
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		SceneManager.LoadScene (scene);
+	}
+}
