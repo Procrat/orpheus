c27119af554dbc6c33468e2b3ffefa9dbc9ce706
Sami Habib <puggsoy@gmail.com>
2017-09-17 15:03:55 +1200

    Halfway with ghost into possessee movement

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -7,10 +7,13 @@ public class Ghost : MonoBehaviour
     public float floatSpeed;
 
     public GameObject playerManager;
+    
+    private bool gonnaPossess = false;
+    private GameObject possessee = null;
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "enemy") {
-            playerManager.SendMessage("Possess", other.gameObject);
+            playerManager.GetComponent<PlayerManager>().BeginPossess(other.gameObject);
         }
         else
         if(other.gameObject.name == "Lady")
@@ -21,10 +24,39 @@ public class Ghost : MonoBehaviour
 
     private void FixedUpdate ()
     {
-        FloatUp();
+        if(gonnaPossess)
+        {
+            MoveTowards();
+        }
+        else FloatUp();
     }
 
     private void FloatUp() {
         transform.Translate (floatSpeed * Vector2.up);
+    }
+    
+    public void GoAndPossess(GameObject possessee)
+    {
+        gonnaPossess = true;
+        this.possessee = possessee;
+    }
+    
+    private void MoveTowards()
+    {
+        Vector2 direction = possessee.transform.position - transform.position;
+        Vector2 moveVector = Vector2.ClampMagnitude(direction, floatSpeed / 10);
+        
+        Debug.Log("ghostpos=" + transform.position + ", enemypos=" + possessee.transform.position);
+        Debug.Log(direction.magnitude);
+        //Debug.Log("moveMagnitude=" + moveVector.magnitude + ", floatSpeed=" + floatSpeed);
+        if(moveVector.magnitude - (floatSpeed / 10) < 0.0001)
+        {
+            gonnaPossess = false;
+            playerManager.GetComponent<PlayerManager>().ActuallyPossess(possessee);
+        }
+        else
+        {
+            transform.Translate(moveVector);
+        }
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,84 +5,95 @@ using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour {
 
-	public GameObject player;
-	public GameObject ghost;
+    public GameObject player;
+    public GameObject ghost;
     public float ghostPerishDelay;  // In seconds
 
-	private Coroutine dieAfterAWhile;
+    private Coroutine dieAfterAWhile;
 
-	public AudioClip deathSound;
-	public AudioClip possessSound;
-	private AudioSource source;
+    private bool gonnaPossess = false;
 
-	void Awake(){
-		source = GetComponent<AudioSource> ();
+    public AudioClip deathSound;
+    public AudioClip possessSound;
+    private AudioSource source;
+
+    void Awake(){
+	source = GetComponent<AudioSource> ();
+    }
+
+    void Update()
+    {
+	if (Input.GetButtonDown ("Cancel")) {
+	    Debug.Log("Quitting level.");
+	    SceneManager.LoadScene("Start");
 	}
 
-	void Update()
-	{
-        if (Input.GetButtonDown ("Cancel")) {
-            Debug.Log("Quitting level.");
-            SceneManager.LoadScene("Start");
-        }
-
-        // Temporary shortcut to win
-        /*if (Input.GetKeyDown ("space")) {
-            Win ();
-        }*/
-	}
+	// Temporary shortcut to win
+	/*if (Input.GetKeyDown ("space")) {
+	  Win ();
+	  }*/
+    }
 
     public void Die ()
     {
-		if (player == ghost) {
-			Debug.Log("Internal error: Tried to die while already being a ghost!");
-			return;
-		}
+	if (player == ghost) {
+	    Debug.Log("Internal error: Tried to die while already being a ghost!");
+	    return;
+	}
 
 
-		source.PlayOneShot (deathSound, 1f);
+	source.PlayOneShot (deathSound, 1f);
 
-        Debug.Log ("Hooray! You die!");
+	Debug.Log ("Hooray! You die!");
 
-		player.GetComponent<Player>().enabled = false;
-        // Don't tag the currently possessed body as an enemy;
-        // otherwise, we'll just possess it again.
-        player.tag = "Untagged";
-        ghost.transform.position = player.transform.position;
-		ghost.SetActive(true);
-		player = ghost;
-        dieAfterAWhile = StartCoroutine (ActuallyDieAfterAWhile ());     
+	player.GetComponent<Player>().enabled = false;
+	// Don't tag the currently possessed body as an enemy;
+	// otherwise, we'll just possess it again.
+	player.tag = "Untagged";
+	ghost.transform.position = player.transform.position;
+	ghost.SetActive(true);
+	player = ghost;
+	dieAfterAWhile = StartCoroutine (ActuallyDieAfterAWhile ());
     }
 
-	public void Possess(GameObject enemy) {
-		
-		source.PlayOneShot (possessSound, 1f);
+    public void BeginPossess(GameObject enemy) {
+	source.PlayOneShot (possessSound, 1f);
+	if(gonnaPossess) return;
 
-		if (player != ghost) {
-			Debug.Log("Internal error: Tried to possess while not being a ghost!");
-			return;
-		}
-
-		Debug.Log("Possessing enemy!");
-
-		ghost.SetActive(false);
-		enemy.GetComponent<WalkerAI>().enabled = false;
-		enemy.GetComponent<WalkerPlayer>().enabled = true;
-		player = enemy;
-		StopCoroutine(dieAfterAWhile);
+	if (player != ghost) {
+	    Debug.Log("Internal error: Tried to possess while not being a ghost!");
+	    return;
 	}
+
+	Debug.Log("Possessing enemy!");
+
+	enemy.GetComponent<WalkerAI>().enabled = false;
+	StopCoroutine(dieAfterAWhile);
+
+	gonnaPossess = true;
+	ghost.GetComponent<Ghost>().GoAndPossess(enemy);
+    }
+
+    public void ActuallyPossess(GameObject enemy)
+    {
+	ghost.SetActive(false);
+	enemy.GetComponent<WalkerPlayer>().enabled = true;
+	player = enemy;
+
+	gonnaPossess = false;
+    }
 
     IEnumerator ActuallyDieAfterAWhile ()
     {
-        yield return new WaitForSeconds (ghostPerishDelay);
-        Debug.Log("Awww. You lost.");
-        // TODO change this once there is a death-scene
-        SceneManager.LoadScene("Died");
+	yield return new WaitForSeconds (ghostPerishDelay);
+	Debug.Log("Awww. You lost.");
+	// TODO change this once there is a death-scene
+	SceneManager.LoadScene("Died");
     }
 
     public void Win ()
     {
-        Debug.Log ("Hooray! You won!");
-        SceneManager.LoadScene ("End");
+	Debug.Log ("Hooray! You won!");
+	SceneManager.LoadScene ("End");
     }
 }
