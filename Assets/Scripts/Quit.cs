deaad305e3f9867c928dc14a3db45c6492ec4fbf
Stijn Seghers <stijnseghers@gmail.com>
2017-09-17 17:01:04 +1200

    Able to quit

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+	void Update () {
+		if (Input.GetButtonDown("Cancel")) {
+			Debug.Log("Quitting. Goodbye!");
+			Application.Quit();
+		}
+	}
+}
