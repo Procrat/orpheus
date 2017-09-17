4b0483603c42b8f8c263df71f7b1a926a0de1fcd
Stijn Seghers <stijnseghers@gmail.com>
2017-09-17 16:39:58 +1200

    Transition 0.5 -> 1

---
+++ Assets/Scripts/DelayedNextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DelayedNextScene : MonoBehaviour {
+
+	public float startDelay;
+	public string scene;
+
+    void Start ()
+    {
+        StartCoroutine (StartAfterDelay ());
+    }
+
+    IEnumerator StartAfterDelay ()
+    {
+        yield return new WaitForSeconds (startDelay);
+        SceneManager.LoadScene (scene);
+    }
+}
